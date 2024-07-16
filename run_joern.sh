40e22458adbe5935dc9403362a16b05a24f8eba7
Zixuan Tan <tanzixuan.me@gmail.com>
2024-07-16 22:24:26 +0800

    4

--- run_joern.sh
+++ run_joern.sh
@@ -1,36 +1,71 @@
 #!/bin/bash
 # usage: $0 project_src revision
+set -e -o pipefail # exit on error
+
 DIR=$(dirname $0)
 SRC=$(realpath $1)
 REV=$2
-OUTPUT=$(realpath ./$1_$REV)
+PROJECT=$(basename $SRC)
+NEO4J_OUTPUT=$DIR/${PROJECT}_${REV}
+mkdir -p $NEO4J_OUTPUT
 
+TMP_SRC=$(mktemp -d || exit -1)
+echo "Copying source code to $TMP_SRC"
 
-WORKSPACE=$(mktemp -d)
-echo "Copying source code to $WORKSPACE"
+pushd $SRC || exit -1
+git restore --staged .
+git restore .
+git clean -dfx
+git checkout $REV
 
-pushd $SRC
-    git restore --staged .
-    git restore .
-    git clean -dfx
-    git checkout $REV
+find . \
+    -type f \
+    -regextype posix-extended \
+    -regex '.*\.(c|h|cc|hh|cpp|cxx|hpp)' \
+    -not -path '*/samples/*' \
+    -not -path '*/testing/*' \
+    -not -path '*/test/*' \
+    -not -path '*/Documentation/*' \
+    -size -450k \
+    -exec sh -c "dst=\"$TMP_SRC/{}\"; mkdir -p \$(dirname \$dst); echo {}; cp {} \$dst" \;
+popd || exit -1
 
-    find . \
-        -type f \
-        -regextype posix-extended \
-        -regex '.*\.(c|h|cc|hh|cpp|cxx|hpp)' \
-        -not -path '*/samples/*' \
-        -not -path '*/testing/*' \
-        -not -path '*/test/*' \
-        -not -path '*/Documentation/*' \
-        -size -450k \
-        -exec sh -c "dst=\"$WORKSPACE/{}\"; mkdir -p \$(dirname \$dst); echo {}; cp {} \$dst" \;
-popd
+TMP_WORKSPACE=$(mktemp -d || exit -1)
 
-exit 0
-rm -rf $OUTPUT
-$DIR/octopus-joern/joern-parse -outformat neo4j -outdir $OUTPUT $WORKSPACE
-rm -rf $WORKSPACE
+pushd $TMP_WORKSPACE || exit -1
+mkdir workspace
+echo "Output cpg to $TMP_WORKSPACE"
+joern-parse $TMP_SRC && rm -rf $TMP_SRC
 
-mkdir -p $OUTPUT/graph.db && mv $OUTPUT/* $OUTPUT/graph.db/
+echo "Exporting cpg as neo4j csv to $TMP_EXPORTED"
+joern-export -o all_neo4jcsv --repr all --format neo4jcsv cpg.bin && rm -rf cpg.bin
+pushd all_neo4jcsv || exit -1
+nodes=$(ls nodes_*_data.csv)
+edges=$(ls edges_*_data.csv)
+for node in $nodes; do
+    node_header=$(echo $node | sed 's/data/header/')
+    params="--nodes $node_header,$node $params"
+done
+for edge in $edges; do
+    edge_header=$(echo $edge | sed 's/data/header/')
+    params="--relationships $edge_header,$edge $params"
+done
+popd || exit -1
+popd || exit -1
 
+echo "Starting neo4j at $NEO4J_OUTPUT"
+container_id=$(./start_neo4j.sh $NEO4J_OUTPUT --volume $TMP_WORKSPACE/all_neo4jcsv:/var/lib/neo4j/import -d)
+
+echo "Container id: $container_id"
+
+sleep 5
+
+echo "Importing cpg to neo4j"
+docker exec $container_id /bin/bash -c "cd /var/lib/neo4j/import \
+&& neo4j-admin database import full --overwrite-destination --multiline-fields=true $params" &&
+    sudo rm -rf $TMP_WORKSPACE
+
+echo "Stopping neo4j"
+docker stop $container_id
+
+echo "Done. Neo4j database is located at $NEO4J_OUTPUT"
--- start_neo4j.sh
+++ start_neo4j.sh
@@ -9,8 +9,12 @@ docker \
     --volume=$(realpath $1):/data \
     --name=neo4j-$FORMULA_NAME \
     --env=NEO4J_AUTH=none \
-    --env=NEO4J_ALLOW_STORE_UPGRADE=true \
-    --env=NEO4J_dbms_allow__upgrade=true \
-    --env=NEO4J_dbms_allow__format__migration=true \
     ${@:2} \
-    neo4j:2.3.12
+    neo4j:5.10.0
+    # neo4j:2.3.12
+
+    # --env=NEO4J_ALLOW_STORE_UPGRADE=true \
+    # --env=NEO4J_dbms_allow__upgrade=true \
+    # --env=NEO4J_dbms_allow__format__migration=true \
+    # --env=NEO4J_server_config_strict__validation_enabled=false \
+    # --volume $(pwd)/xxx:/var/lib/neo4j/import
